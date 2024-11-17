5057d9287c7c730b423736df3bd8f47a73345d0b
Dodul01 <mhdodul299@gmail.com>
2024-11-17 13:51:34 +0600

    Server set up completed

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import app from './app';
+import config from './app/config';
+import mongoose from 'mongoose';
+
+async function main() {
+  // URI const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@testybyte.8ypqlqr.mongodb.net/?retryWrites=true&w=majority&appName=testyByte`;
+  // DB_PASS=testuser
+  // DB_USER=testuser
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`Server is running on local host ${config.port}.`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
